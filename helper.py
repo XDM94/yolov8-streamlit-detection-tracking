d7beca62fccced8e09e1c5e417e8c119d89589cd
Denis Khlamov <sturmtrooper94@gmail.com>
2024-04-12 11:20:22 +0300

    app.py Проверил код линтером, внес небольшие изменения, изменил название парочки переменных.

--- helper.py
+++ helper.py
@@ -22,15 +22,16 @@ def load_model(model_path):
 
 
 def display_tracker_options():
-    display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
-    is_display_tracker = True if display_tracker == 'Yes' else False
+    display_tracker = st.radio("Display Tracker", ("Yes", "No"))
+    is_display_tracker = True if display_tracker == "Yes" else False
     if is_display_tracker:
         tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
         return is_display_tracker, tracker_type
     return is_display_tracker, None
 
 
-def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
+def _display_detected_frames(
+    conf, model, st_frame, image, is_display_tracking=None, tracker=None):
     """
     Display the detected objects on a video frame using the YOLOv8 model.
 
@@ -46,7 +47,7 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
     """
 
     # Resize the image to a standard size
-    image = cv2.resize(image, (720, int(720*(9/16))))
+    image = cv2.resize(image, (720, int(720 * (9 / 16))))
 
     # Display object tracking, if specified
     if is_display_tracking:
@@ -57,11 +58,9 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
 
     # # Plot the detected objects on the video frame
     res_plotted = res[0].plot()
-    st_frame.image(res_plotted,
-                   caption='Detected Video',
-                   channels="BGR",
-                   use_column_width=True
-                   )
+    st_frame.image(
+        res_plotted, caption="Detected Video", channels="BGR", use_column_width=True
+    )
 
 
 def play_youtube_video(conf, model):
@@ -82,23 +81,24 @@ def play_youtube_video(conf, model):
 
     is_display_tracker, tracker = display_tracker_options()
 
-    if st.sidebar.button('Detect Objects'):
+    if st.sidebar.button("Detect Objects"):
         try:
             yt = YouTube(source_youtube)
             stream = yt.streams.filter(file_extension="mp4", res=720).first()
             vid_cap = cv2.VideoCapture(stream.url)
 
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker,
-                                             )
+                    _display_detected_frames(
+                        conf,
+                        model,
+                        st_frame,
+                        image,
+                        is_display_tracker,
+                        tracker,
+                    )
                 else:
                     vid_cap.release()
                     break
@@ -121,22 +121,20 @@ def play_rtsp_stream(conf, model):
         None
     """
     source_rtsp = st.sidebar.text_input("rtsp stream url:")
-    st.sidebar.caption('Example URL: rtsp://admin:12345@192.168.1.210:554/Streaming/Channels/101')
+    st.sidebar.caption(
+        "Example URL: rtsp://admin:12345@192.168.1.210:554/Streaming/Channels/101"
+        )
     is_display_tracker, tracker = display_tracker_options()
-    if st.sidebar.button('Detect Objects'):
+    if st.sidebar.button("Detect Objects"):
         try:
             vid_cap = cv2.VideoCapture(source_rtsp)
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
+                    _display_detected_frames(
+                        conf, model, st_frame, image, is_display_tracker, tracker
+                    )
                 else:
                     vid_cap.release()
                     # vid_cap = cv2.VideoCapture(source_rtsp)
@@ -164,20 +162,21 @@ def play_webcam(conf, model):
     """
     source_webcam = settings.WEBCAM_PATH
     is_display_tracker, tracker = display_tracker_options()
-    if st.sidebar.button('Detect Objects'):
+    if st.sidebar.button("Detect Objects"):
         try:
             vid_cap = cv2.VideoCapture(source_webcam)
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker,
-                                             )
+                    _display_detected_frames(
+                        conf,
+                        model,
+                        st_frame,
+                        image,
+                        is_display_tracker,
+                        tracker,
+                    )
                 else:
                     vid_cap.release()
                     break
@@ -199,31 +198,25 @@ def play_stored_video(conf, model):
     Raises:
         None
     """
-    source_vid = st.sidebar.selectbox(
-        "Choose a video...", settings.VIDEOS_DICT.keys())
+    source_vid = st.sidebar.selectbox("Choose a video...", settings.VIDEOS_DICT.keys())
 
     is_display_tracker, tracker = display_tracker_options()
 
-    with open(settings.VIDEOS_DICT.get(source_vid), 'rb') as video_file:
+    with open(settings.VIDEOS_DICT.get(source_vid), "rb") as video_file:
         video_bytes = video_file.read()
     if video_bytes:
         st.video(video_bytes)
 
-    if st.sidebar.button('Detect Video Objects'):
+    if st.sidebar.button("Detect Video Objects"):
         try:
-            vid_cap = cv2.VideoCapture(
-                str(settings.VIDEOS_DICT.get(source_vid)))
+            vid_cap = cv2.VideoCapture(str(settings.VIDEOS_DICT.get(source_vid)))
             st_frame = st.empty()
-            while (vid_cap.isOpened()):
+            while vid_cap.isOpened():
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
+                    _display_detected_frames(
+                        conf, model, st_frame, image, is_display_tracker, tracker
+                    )
                 else:
                     vid_cap.release()
                     break
